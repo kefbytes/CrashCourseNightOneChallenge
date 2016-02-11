fc69453d72c8701b6561134dfd20d2cfa856ee5d
kent@kefbytes.com <kent@kefbytes.com>
2016-02-11 13:21:51 -0500

    First Commit

---
+++ CrashCourseNightOneChallenge/LoginViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewModel.swift
+//  SwiftCrashCourseApp
+//
+//  Created by Franks, Kent on 11/5/15.
+//  Copyright © 2015 Franks, Kent. All rights reserved.
+//
+
+import Foundation
+import LocalAuthentication
+
+
+class LoginViewModel {
+    
+}
